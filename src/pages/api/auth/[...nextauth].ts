3f8f49baa4a49749f3f63b9222aee7a589530d3c
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2023-01-24 22:43:57 -0300

    Configurando gravando email do usuario logado no BD e criação da logica de validação de usuario ja existente

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
@@ -13,5 +17,24 @@ export const authOptions = {
       },
     }),
   ],
-};
-export default NextAuth(authOptions);
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email!)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email!)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { GetStaticProps } from "next";
 
 import styles from "./home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
-import { stripe } from "../components/services/stripe";
+import { stripe } from "../services/stripe";
 
 interface HomeProps {
   product: {
